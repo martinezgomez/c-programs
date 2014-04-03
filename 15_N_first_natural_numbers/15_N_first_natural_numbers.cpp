3035ea2076f71b7676fdf043b45156e3f5e8b320
martinezgomez <enriquemartinez.ciclos@gmail.com>
2014-04-03 21:06:15 +0200

    16 incomplete

--- 15_N_first_natural_numbers/15_N_first_natural_numbers.cpp
+++ 15_N_first_natural_numbers/15_N_first_natural_numbers.cpp
@@ -1,22 +1,28 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 7
+//Imprimir el producto de los N numeros naturales
 
-int products(int number){
-  if (number <= 0)
-    return 0;
+//En primer lugar definimos la cantidad de numeros
 
-  printf(" %i ", number);
+#define N 12
 
-  return number * products(number-1);
+//despues creamos una funcion para crear la recursividad
 
+int producto(int cantidad){
+
+  //aqui el estamos diciendo que si la cantidad de numeros es diferente de 1
+  //multiplique los numeros entre si y sino que imprima el numero 1
+
+  return cantidad != 1 ? cantidad * producto(cantidad-1) : cantidad = 1;
 }
 
-int main(int argc, char *argv[]){
+//Ahora creamos el main para imprimir el resultado dentro
 
-  printf("= %i\n", products(N));
+int main (int argc, char *argv[]){
 
+  printf("\n\tEl producto de los %i primeros numeros naturales es: %i\n\n"
+          , N, producto(N));
   return EXIT_SUCCESS;
+
 }
---
+++ 16_print_branckguard_rucirsively/16_print_backguard_recursively.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *sentence = "Tho World Is a Vampire.";
+int letter(int n){
+
+  while(*letter != '\0');
+  return
+    letter(n-1);
+  printf("%s ", sentence);
+
+}
+
+int main (int argc, char *argv[]){
+
+  printf("\n");
+  return EXIT_SUCCESS;
+
+}
